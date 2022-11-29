1244a4413c0e6b3da291ef55b5ad41e6a7cc0ccc
Marco Lechner, GeoBoink <marco@marcolechner.de>
2022-11-29 23:39:33 +0100

    added smtp/starttls/unsigned support

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ Create random pairs from a list of participants, e.g. for xmas
 import sys
 import re
 import random
+import smtplib, ssl
+from getpass import getpass
+
 
 # Make a regular expression for validating an Email
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+# configure Mailbackend
+port = 587 #starttls
+smtp_server = "smtp.example.com"
+sender = "sender@example.com"
 
 def main():
     args = sys.argv[1:]
@@ -28,13 +35,16 @@ def main():
             sys.exit(1)
         if arg not in maillist:
             maillist.append(arg)
+    password = getpass( 'Type your Mailserver password an press enter: ' )
+    # Create a secure SSL context
+    #context = ssl.create_default_context()
+    context = ssl._create_unverified_context() # self-signed cert
+
     print("Used: E-Mails: " + ", ".join(maillist))
     print("Result:")
     for p in pairlist(maillist):
-        print(p[0] + " beschenkt " + p[1])
+        sendmail(donor=p[0], donee=p[1], password=password, context=context, sender=sender)
 
- 
-# Define a function for for validating an Email
 def check(email):
     # pass the regular expression
     # and the string into the fullmatch() method
@@ -54,6 +64,28 @@ def pairlist(shufflelist):
             random.shuffle(shufflelist)
         pairlist.append((k, shufflelist.pop(0)))
     return pairlist
+
+def sendmail(donor, donee, password, context, sender):
+    message = """Subject: Xmas Roulette
+To: {recipient}
+From : {sender}
+Hi {name}, thanks for participating! you are selected to present {recipient} with something appropriate!"""
+    sender = sender
+    with smtplib.SMTP(smtp_server, port) as server:
+        # Try to log in to server and send email
+        try:
+            server.ehlo() # Can be omitted
+            server.starttls(context=context) # Secure the connection
+            server.ehlo() # Can be omitted
+            server.login(sender, password)
+            # Send email here
+            server.sendmail(sender, donor, message.format(name=donor, recipient=donee, sender=sender))
+        except Exception as e:
+            # Print any error messages to stdout
+            print(e)
+        finally:
+            server.quit() 
+        print(f'Sent to {donor}')
 
 
 if __name__ == '__main__':
